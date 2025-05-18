abc6c208162919aa3ffef9465a3e90c2957c454f
Manojbabu <83714923+manusoft@users.noreply.github.com>
2025-05-18 17:11:08 +0400

    Add Console.Sample project and update solution structure
    - Updated `FlexiMap.sln` to include new projects: `test`, `sample`, and `Console.Sample`.
    - Created `Console.Sample.csproj` targeting .NET 9.0 with a reference to `FlexiMap`.
    - Added `Program.cs` to demonstrate property mapping from `Source` to `Destination` using `MappingConfiguration`.

---
+++ sample/Console.Sample/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FlexiMap;
+
+Console.WriteLine("Hello, World!");
+
+var mappingConfig = new MappingConfiguration()
+            .ForTypes<Source, Destination>()
+            .ExcludeProperty("Id") // Exclude Id property from mapping
+            .MapProperty("Name", "Name")
+            .MapProperty("Description", "Description");
+
+var source = new Source
+{
+    Id = 1,
+    Name = "Source Name",
+    Description = "Source Description"
+};
+
+var destination = source.Map<Destination>(config: mappingConfig);
+
+Console.WriteLine($"Name: {destination.Name}");
+Console.WriteLine($"Description: {destination.Description}");
+
+// Source class
+public class Source
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
+
+
+// Destination class
+public class Destination
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
